fd49eee4cce14e9b75e90b53850a3bd7e8b96e99
AndresBarriga <andresbarrigaru@gmail.com>
2024-03-03 10:37:15 +0100

    some new changes

--- routes/profileRoutes/authenticateToken.js
+++ routes/profileRoutes/authenticateToken.js
@@ -4,7 +4,6 @@ import jwt from 'jsonwebtoken'
 function authenticateToken(req, res, next) {
   
   const authHeader = req.headers['authorization'];
-  
   const token = authHeader && authHeader.split(' ')[1];
   
   if (token == null) return res.sendStatus(401); // if there isn't any token send 401
